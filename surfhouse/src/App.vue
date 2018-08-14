7833bb72e1c65d416b141208f48b9e9b31d20c84
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-08-14 08:44:58 +0200

    Added footer component

--- surfhouse/src/App.vue
+++ surfhouse/src/App.vue
@@ -2,18 +2,23 @@
   <div id="app">
     <header-comp></header-comp>
     <img src="./assets/logo.png">
+    
     <router-view/>
+
+    <footer-comp></footer-comp>
   </div>
 </template>
 
 <script>
 
 import _Header from '@/components/_Header'
+import _Footer from '@/components/_Footer'
 
 export default {
   name: 'App',
   components: {
-    'header-comp': _Header
+    'header-comp': _Header,
+    'footer-comp': _Footer
   }
 }
 </script>
---
+++ surfhouse/src/components/_Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <section>
+        <footer>
+            <h1>
+                This is the footer
+            </h1>
+        </footer>
+    </section>
+</template>
