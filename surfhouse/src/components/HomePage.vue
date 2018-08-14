4b04413a1904d5f15eac2ee78dbf6649a32b90f5
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-08-14 09:52:12 +0200

    Added method for getProducts, lifecycle prompt for using method and array for data population with markup in router view

--- surfhouse/src/components/HomePage.vue
+++ surfhouse/src/components/HomePage.vue
@@ -3,12 +3,66 @@
         <h1>
             HomePage
         </h1>
+        <div class="productGallery">
+                <div class="product" v-for="product in products" :key="product.id">
+            <router-link :to="/productpage/ + product.id">
+                <div style="border:2px solid pink;">
+                        <span>
+                            {{product.rating}}
+                        </span>                    
+                        <img v-bind:src="product.imgSide"/>
+                    <p style="text-transform: uppercase;">
+                        {{product.title}}
+                    </p>
+                    <p style="text-transform: uppercase;">
+                        {{product.type}}
+                    </p>
+                    <p>
+                        {{product.description}}
+                    </p>                    
+                    <p>
+                        $ {{product.price}}
+                    </p>                    
+                    <div class="productBtn">
+                    buy now
+                </div>
+                </div>
+            </router-link>
+        </div>
+        </div>
+        hamster
     </section>
 </template>
 
 <script>
+
+import axios from "axios";
+
 export default {
-    name: 'HomePage'
+    name: 'HomePage',
+    // Array of product date retrieved on page creation with getProducts method
+    data: function() {
+        return {
+            products: []
+        };
+    },
+    // calls the getProducts method when page is created
+    created: function() {
+        this.getProducts();
+    },
+    methods: {
+        // Add a method for retrieving product data using axios
+        getProducts: function() {
+            axios
+            .get("/static/products.json")
+            .then(response => {
+                this.products = response.data;
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
+    }
 }
 </script>
 
--- surfhouse/src/router/index.js
+++ surfhouse/src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import HomePage from '@/components/HomePage'
 import ProductPage from '@/components/ProductPage'
 import ContactPage from '@/components/ContactPage'
 
 Vue.use(Router)
+
+
 
 export default new Router({
   routes: [
