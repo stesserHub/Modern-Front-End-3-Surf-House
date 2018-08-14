5cc5e6cd8008d4ee43b693cb085fc95d0332e332
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-08-14 14:35:16 +0200

    partial markup product page

--- surfhouse/src/components/ProductPage.vue
+++ surfhouse/src/components/ProductPage.vue
@@ -1,9 +1,104 @@
 <template>
     <section>
+
+        <img v-bind:src="product.img1"/>
+        <img v-bind:src="product.img2"/>
+        <img v-bind:src="product.img3"/>
+
         <h1>
-            Excellent product {{product.price}}
+            {{product.title}}
         </h1>
-         
+
+        <b>
+            €. {{product.price}}
+        </b>
+        <br>
+        <b class="altprice" v-if="product.altprice != null">
+            €. {{product.altprice}}
+        </b>
+
+        <h3>
+            quick overview
+        </h3>
+        <p>
+            {{product.overview}}
+        </p>
+
+        <h3>
+            size
+        </h3>
+        <ul>
+            <li>
+                {{product.size}}
+            </li>
+        </ul>
+
+        <h3>
+            length
+        </h3>
+        <ul>
+            <li>
+                {{product.length}}
+            </li>
+        </ul>
+
+        <div class="cartinteract">
+            <form action="post">
+                <label for="quantity" class="quantity">
+                    quantity:
+                </label>
+                <br>
+                <input type="number" name="quantity" min="1" value="1">
+                <input type="submit" value="add to cart">
+            </form>
+            <ul>
+                <li>
+                    <button>
+                        + Add to Wishlist
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        + Add to Compare
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        + Email to a Friend
+                    </button>
+                </li>
+            </ul>
+        </div>
+
+        <h4>
+            product description
+        </h4>
+        <p>
+            {{product.description}}
+        </p>
+
+        <h4>
+            additional information
+        </h4>
+        <p>
+            {{product.information}}
+        </p>
+
+        <h4>
+            reviews
+        </h4>
+        <p>
+            {{product.review}}
+        </p>
+
+        <h4>
+            product tags
+        </h4>
+        <p>
+            {{product.tags}}
+        </p>
+        
+
     </section>
 </template>
 
