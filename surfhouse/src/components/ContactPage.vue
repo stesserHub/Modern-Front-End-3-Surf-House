f367ada98600fbc309e9e3fa8de198b5907076ac
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-08-14 12:58:23 +0200

    Set up contact form

--- surfhouse/src/components/ContactPage.vue
+++ surfhouse/src/components/ContactPage.vue
@@ -3,8 +3,41 @@
         <h1>
             contact us
         </h1>
+        <p>
+            If you have questions about your order or need any general information our customer service team will be happy to assist you    
+        </p>
         <form action="">
-            <input type="submit">
+            <label for="customername">
+                full name*
+            </label>
+            <br>
+            <input type="text" name="customername">
+            <br>
+            <label for="customeremail">
+                email*
+            </label>
+            <br>
+            <input type="email" name="customeremail">
+            <br>
+            <label for="company">
+                company
+            </label>
+            <br>
+            <input type="text" name="company">
+            <br>
+            <label for="subject">
+                subject*
+            </label>
+            <br>
+            <input type="text" name="subject">
+            <br>            
+            <label for="message">
+                message
+            </label>
+            <br>
+            <textarea  rows="10" cols="30" name="message"/>
+            <br>
+            <input type="submit" name="message">
         </form>
     </section>
 </template>
