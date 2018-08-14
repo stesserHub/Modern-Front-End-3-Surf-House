0726c01b821b57fd434b2229a3f261a7077b41e6
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-08-14 12:35:32 +0200

    set up home render of products

--- surfhouse/src/components/HomePage.vue
+++ surfhouse/src/components/HomePage.vue
@@ -3,28 +3,28 @@
         <h1>
             HomePage
         </h1>
+        <!-- render 6 products from the product array -->
         <div class="productGallery">
-                <div class="product" v-for="product in products" :key="product.id">
-            <router-link :to="/productpage/ + product.id">
-                <div style="border:2px solid pink;">
-                        <div class="label">
+            <div class="product" v-for="product in products.slice(0, 6)" :key="product.id">
+                <router-link :to="/productpage/ + product.id">
+                    <div style="border:2px solid pink;">
+                        <div class="label" v-if="product.label != null">
                             {{product.label}}
                         </div>                    
                         <img v-bind:src="product.img1"/>
-                    <p style="text-transform: uppercase;">
-                        {{product.title}}
-                    </p>                
-                    <p>
-                        €. {{product.price}}
-                    </p>
-                    <p style="text-decoration: line-through;">
-                        €. {{product.altprice}}
-                    </p>
-                </div>
-            </router-link>
+                        <p style="text-transform: uppercase;">
+                            {{product.title}}
+                        </p>                
+                        <p>
+                            €. {{product.price}}
+                        </p>
+                        <p class="altprice" v-if="product.altprice != null">
+                            €. {{product.altprice}}
+                        </p>
+                    </div>
+                </router-link>
+            </div>
         </div>
-        </div>
-        hamster
     </section>
 </template>
 
