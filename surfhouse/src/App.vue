800936adbaca23bc078c747b88f8b35f7524162a
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-08-15 15:51:28 +0200

    set up content area with aside and article

--- surfhouse/src/App.vue
+++ surfhouse/src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <div id="app">
-    <header-comp></header-comp>
-    <menu-comp></menu-comp>
-    <tag-comp></tag-comp>
-    
-    <router-view/>
+<div id="app">
+  <header-comp></header-comp>
 
-    <brand-comp></brand-comp>
-    <footer-comp></footer-comp>
-    <svg-data></svg-data>
-  </div>
+  <section id="content">
+    <aside>
+      <menu-comp></menu-comp>
+      <tag-comp></tag-comp>
+    </aside>
+    <article>
+      <router-view/>
+    </article>
+  </section>
+  
+  <brand-comp></brand-comp>
+  <footer-comp></footer-comp>
+  <svg-data></svg-data>
+</div>
 </template>
 
 <script>
@@ -22,15 +28,15 @@ import _Footer from '@/components/_Footer'
 import _SVG from '@/components/_SVG'
 
 export default {
-  name: 'App',
-  components: {
-    'header-comp': _Header,
-    'menu-comp': _Menu,
-    'tag-comp': _Tags,
-    'brand-comp': _Brands,
-    'footer-comp': _Footer,
-    'svg-data': _SVG
-  }
+name: 'App',
+components: {
+  'header-comp': _Header,
+  'menu-comp': _Menu,
+  'tag-comp': _Tags,
+  'brand-comp': _Brands,
+  'footer-comp': _Footer,
+  'svg-data': _SVG
+}
 }
 </script>
 
