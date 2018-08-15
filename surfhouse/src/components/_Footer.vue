4e6f28f0f349ab0bf3271922348564e402d94c90
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-08-15 13:15:18 +0200

    partial styling footer

--- surfhouse/src/components/_Footer.vue
+++ surfhouse/src/components/_Footer.vue
@@ -1,9 +1,140 @@
 <template>
     <section>
         <footer>
-            <h1>
-                This is the footer
-            </h1>
+            <div class="footer-parent">
+                <div class="footer-child list">
+                    <h5>
+                        category
+                    </h5>
+                    <ul>
+                        <a href="#">
+                            <li>
+                                home
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                about us
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                eshop
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                features
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                new collections
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                blog
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                contact
+                            </li>
+                        </a>
+                    </ul>                    
+                </div>
+                <div class="footer-child list">
+                    <h5>
+                        our account
+                    </h5>
+                    <ul>
+                        <a href="#">
+                            <li>
+                                your account
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                personal information
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                addresses
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                discount
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                orders hsitory
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                addresses
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                search terms
+                            </li>
+                        </a>
+                    </ul>                    
+                </div>
+                <div class="footer-child list">
+                    <h5>
+                        our support
+                    </h5>
+                    <ul>
+                        <a href="#">
+                            <li>
+                                site map
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                search terms
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                advanced search
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                mobile
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                contact us
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                mobile
+                            </li>
+                        </a>
+                        <a href="#">
+                            <li>
+                                addresses
+                            </li>
+                        </a>
+                    </ul>                    
+                </div>
+                <div class="footer-child">
+                    newsletter
+                </div>
+                <div class="footer-child">
+                    about us
+                </div>
+            </div>
         </footer>
     </section>
 </template>
