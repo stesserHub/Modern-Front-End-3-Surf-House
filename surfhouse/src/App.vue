640e4b478e8f76d0e02dc3ba698a374e6bc73ccf
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-08-15 15:29:12 +0200

    added and styled the brands section

--- surfhouse/src/App.vue
+++ surfhouse/src/App.vue
@@ -6,6 +6,7 @@
     
     <router-view/>
 
+    <brand-comp></brand-comp>
     <footer-comp></footer-comp>
     <svg-data></svg-data>
   </div>
@@ -16,6 +17,7 @@
 import _Header from '@/components/_Header'
 import _Menu from '@/components/_Menu'
 import _Tags from '@/components/_Tags'
+import _Brands from '@/components/_Brands'
 import _Footer from '@/components/_Footer'
 import _SVG from '@/components/_SVG'
 
@@ -25,6 +27,7 @@ export default {
     'header-comp': _Header,
     'menu-comp': _Menu,
     'tag-comp': _Tags,
+    'brand-comp': _Brands,
     'footer-comp': _Footer,
     'svg-data': _SVG
   }
---
+++ surfhouse/src/components/_Brands.vue
@@ -0,0 +1,39 @@
+<template>
+    <section>
+        <div id="brand-parent">
+            <div class="brand-child">
+                <img id="logo" src="../../static/brand.png"/>
+            </div>
+            <div class="brand-child">
+                <img id="logo" src="../../static/brand.png"/>
+            </div>
+            <div class="brand-child">
+                <img id="logo" src="../../static/brand.png"/>
+            </div>
+            <div class="brand-child">
+                <img id="logo" src="../../static/brand.png"/>
+            </div>
+            <div class="brand-child">
+                <img id="logo" src="../../static/brand.png"/>
+            </div>
+            <div class="brand-child">
+                <img id="logo" src="../../static/brand.png"/>
+            </div>
+            <div class="brand-child">
+                <img id="logo" src="../../static/brand.png"/>
+            </div>
+            <div class="brand-child">
+                <img id="logo" src="../../static/brand.png"/>
+            </div>
+            <div class="brand-child">
+                <img id="logo" src="../../static/brand.png"/>
+            </div>
+            <div class="brand-child">
+                <img id="logo" src="../../static/brand.png"/>
+            </div>
+            <div class="brand-child">
+                <img id="logo" src="../../static/brand.png"/>
+            </div>
+        </div>
+    </section>
+</template>
