7e9c37ea1d0f200b00c7640f08bf4e12df2800e3
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-08-15 09:09:22 +0200

    markup og cart and partial stylinf

--- surfhouse/src/components/_Header.vue
+++ surfhouse/src/components/_Header.vue
@@ -5,6 +5,49 @@
                 This is the header test
             </h1>
             <img src="../assets/SurfhouseLogo.png">
+            <div id="cart">
+                <div class="cartlink">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                log in
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                wish list(0)
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="cartcontent">
+                    <img src="#" alt=""/>
+                    <p class="totalcart">
+                        €. 1234.56
+                    </p>
+                    <p class="itemcount">
+                        99 items
+                    </p>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                view cart
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                checkout
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="cartsearch">
+                    <form action="oist">
+                        <input type="text" name="search" placeholder="search">
+                        <input type="submit" value="O">
+                    </form>
+                </div>
+            </div>
         </header>
     </section>
 </template>
