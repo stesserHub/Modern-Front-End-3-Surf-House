0816b2db79f1369cedaccf6971f0b0d54351b122
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-08-16 16:28:49 +0200

    how do you 'npm i bootstrap-vue'???

--- surfhouse/src/router/index.js
+++ surfhouse/src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
+// import BootstrapVue from 'bootstrap-vue';
 import Router from 'vue-router';
 import HomePage from '@/components/HomePage';
 import ProductPage from '@/components/ProductPage';
 import ContactPage from '@/components/ContactPage';
 import VeeValidate from 'vee-validate';
 import {Tabs, Tab} from 'vue-tabs-component';
+// import { Carousel } from 'bootstrap-vue/es/components';
+
+// Vue.use(BootstrapVue);
+// Vue.use(Carousel);
 
 Vue.use(VeeValidate);
 Vue.use(Router);
